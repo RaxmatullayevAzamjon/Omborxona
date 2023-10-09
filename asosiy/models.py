ae9b1a212cf70a291c77fd6399b176a79cd5e0ad
RaxmatullayevAzamjon <raxmatullayevazamjon025@gmail.com>
2023-10-09 17:05:43 +0500

    Added a view for

--- asosiy/models.py
+++ asosiy/models.py
@@ -1,3 +1,40 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+class Ombor(AbstractUser):
+    # id, usename, password, is_staff, is_superuser, is_active
+    nom = models.CharField(max_length=30)
+    manzil = models.CharField(max_length=150)
+    ism = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nom
+
+
+d = (
+    ("Kg","Kg"),
+    ("dona","dona"),
+    ("litr","litr")
+)
+class Maxsulot(models.Model):
+    nom = models.CharField(max_length=30)
+    narx = models.PositiveSmallIntegerField()
+    miqdor = models.PositiveSmallIntegerField()
+    olchov = models.CharField(max_length=30,choices=d)
+    ombor = models.ForeignKey(Ombor,on_delete=models.CASCADE)
+    brend = models.CharField(max_length=20,null=True,blank=True)
+
+    def __str__(self):
+        return self.nom
+
+class Mijoz(models.Model):
+    nom = models.CharField(max_length=30)
+    manzil = models.CharField(max_length=30)
+    ism = models.CharField(max_length=30)
+    tel = models.CharField(max_length=20)
+    ombor = models.ForeignKey(Ombor,on_delete=models.CASCADE)
+    qarz = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.nom
+
--- asosiy/views.py
+++ asosiy/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import logout, authenticate,login
+from django.views import View
+from .models import *
 
-# Create your views here.
+class Login(View):
+    def get(self,request):
+        return render(request,"home.html")
+
+    def post(self, request):
+        user = authenticate(
+            username = request.POST.get("login"),
+            password = request.POST.get("parol")
+        )
+        if user is not None:
+            login(request, user)
+            return redirect("/bolimlar/")
+        return redirect("/")
+
+def bolimlar(request):
+    return render(request,"bulimlar.html")
+
+class Logout(View):
+    def get(self, request):
+        logout(request)
+        return redirect("/")
+
+class Products(View):
+    def get(self, request):
+        content = {
+            "products": Maxsulot.objects.filter(ombor=request.user)
+        }
+        return render(request, "products.html",content)
+    def post(self, request):
+        Maxsulot.objects.create(
+            nom = request.POST.get("n"),
+            narx = request.POST.get("narx"),
+            miqdor = request.POST.get("m"),
+            brend = request.POST.get("b"),
+            olchov = request.POST.get("o"),
+            ombor = Ombor.objects.get(id=request.user.id)
+        )
+        return redirect("/products/")
+
+class MijozView(View):
+    def get(self, request):
+        content = {
+        "mijozlar": Mijoz.objects.filter(ombor=request.user)
+        }
+        return render(request, "clients.html",content)
+
+def Mijoz_ochir(request, son):
+    Mijoz.objects.filter(id=son).delete()
+    return redirect("/mijoz/")
